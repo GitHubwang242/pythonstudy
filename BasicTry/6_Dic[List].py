a7fd20b727dfd5ae67477c2e55e0daae85a965e2
GitHubwang242 <54611931+GitHubwang242@users.noreply.github.com>
2023-10-14 17:47:52 +0800

    20231014

--- BasicTry/6_Dic[List].py
+++ BasicTry/6_Dic[List].py
@@ -2,11 +2,11 @@ pizza = {
     'crust': 'thick',
     'toppings': ['mushrooms', 'extra cheese'],
 }
-
-print(f"You ordered a {pizza['crust']}-crust pizza " 
-      "with the following toppings:")
-for topping in pizza['toppings']:
-    print(f"\t"+topping)
+#
+# print(f"You ordered a {pizza['crust']}-crust pizza "
+#       "with the following toppings:")
+# for topping in pizza['toppings']:
+#     print(f"\t"+topping)
 
 
 favorite_languages = {
@@ -15,12 +15,147 @@ favorite_languages = {
     'edward': ['ruby', 'go'],
     'phil': ['python', 'haskell'],
 }
-for name, languages in favorite_languages.items():
+# for name, languages in favorite_languages.items():
+#
+#     if len(languages) >1:
+#         print(f"\n{name.title()}'s favorite languages are:")
+#         for language in languages:
+#             print(f"\t{language.title()}")
+#     else:
+#         print(f"\n{name.title()}'s favorite languages is:")
+#         print(f"\t{languages[0]}")
 
-    if len(languages) >1:
-        print(f"\n{name.title()}'s favorite languages are:")
-        for language in languages:
-            print(f"\t{language.title()}")
-    else:
-        print(f"\n{name.title()}'s favorite languages is:")
-        print(f"\t{languages[0]}")
+
+# 练习6-7 人们
+people = {
+    'wang':{
+        'age': 1,
+        'sex': 'nan',
+    },
+    'miao':{
+        'age': 2,
+        'sex': 'nv',
+    },
+    'ban': {
+        'age':3,
+        'sex': 'nv',
+    },
+}
+
+# for name, name_info in people.items():
+#     print(f"name is : {name}")
+#     age = name_info['age']
+#     sex = name_info['sex']
+#     print(f"the age is {age}")
+#     print(f"the sex is {sex}")
+
+
+# 练习6-8
+cat = {'name':'mimi','host_name': 'miaomiao'}
+dog = {'name':'wangwang', 'host_name': 'banban'}
+duck = {'name':'gaga', 'host_name': '17'}
+pets = [ cat, dog, duck]
+# for pet in pets:
+    # print(pet)
+    # print(pet['name'])
+    # print(pet['host_name'])
+    # print(pet.items())
+    # print(pet.keys())
+    # print(pet.values())
+    # for name,host_name in pet.items():
+    #     print(f"\n name is {name[0]},host_name is {host_name[0]}")
+        # print(f"host_name is {host_name}")
+        # print(f"\n name is {name[1]},host_name is {host_name[1]}")
+
+print("**********************************")
+# print(pets[0].keys())
+
+# 练习6-9
+
+favorite_places1 = {
+    'zhao':{
+        'f':'beijing',
+        's':'tianjin',
+        't':'hebei',
+    },
+    'qian':{
+        'f':'beijing',
+        's':'tianjin',
+        't':'hebei',
+    },
+    'sun':{
+        'f':'beijing',
+        's':'tianjin',
+        't':'hebei',
+    },
+}
+favorite_places2 = {
+    'zhao':['beijing','tianjin','hebei'],
+    'qian':['beijing','tianjin','hebei'],
+    'sun':['beijing','tianjin','hebei'],
+}
+
+for name,place in favorite_places1.items():
+    print(name)
+    place1 = place['f'],
+    place2 = place['s'],
+    place3 = place['t'],
+    print(place1)
+    print(place2)
+    print(place3)
+    print("******************")
+
+for name,value in favorite_places2.items():
+    print("-------------------------")
+    print(name)
+    for va in value:
+        print(va)
+
+# 练习6-10
+num_dic = {
+    'Zhao' : [1,2,3,4],
+    'Qian' : [5,6,7,8],
+}
+
+# 练习6-11
+cities = {
+    'beijing1':{
+        'country':'China',
+        'population':1000000,
+        'fact':"capital",
+    },
+    'beijing2':{
+        'country':'China',
+        'population':2000000,
+        'fact':"capital",
+    },
+    'beijing3':{
+        'country':'China',
+        'population':3000000,
+        'fact':"capital",
+    },
+}
+print('++++++++++++++++++++++++++++++++')
+for name,info in cities.items():
+    print(name)
+    print(info['country'])
+    print(info['population'])
+    print(info['fact'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ BasicTry/7_input_while.py
@@ -0,0 +1,60 @@
+# message = input("Tell me something, and I will repeat it back to you:")
+# print(message)
+
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name?"
+
+# name = input(prompt)
+# print(f"\n Hello,{name}!")
+
+# 使用int（）获取数值输入
+# age = input("How old are you? ")
+# age = int(age)
+# print(type(age))
+
+# height = input("How tall are you, in inches? ")
+# height = int(height)
+# if height >= 48:
+#     print("\nYou're tall enough to ride!")
+# else:
+#     print("\nYou'll be able to ride when you're a little older.")
+
+
+# number = input("Enter a number, and I'll tell you if it's even or odd: ")
+# number = int(number)
+# if number % 2 == 0:
+#     print(f"\nThe number {number} is even.")
+# else:
+#     print(f"\nThe number {number} is odd.")
+#
+
+
+# 练习7-1
+# type = input("which car you want?")
+# print(f"Let me see if I can find you a {type}")
+
+# 练习7-2
+# num = int(input("How many people will eating?"))
+# if num >8 :
+#     print("没有空桌")
+# else:
+#     print("有空桌")
+
+# 练习7-3
+# num = int(input("输入数字："))
+# if num%10 == 0:
+#     print("当前数字是10的倍数")
+# else:
+#     print("当前数字不是10的倍数")
+#
+
+
+
+
+
+
+
+
+
+
+
